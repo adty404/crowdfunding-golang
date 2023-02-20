23b331d06ddabb8b154518fb303e9946a06e8e60
Aditya Prasetyo <adty404@gmail.com>
2023-02-20 15:49:44 +0700

    JWT Authentication (Generate Token)

---
+++ auth/service.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Service interface {
+	GenerateToken(userID int) (string, error)
+}
+
+type jwtService struct {
+}
+
+func NewService() *jwtService {
+	return &jwtService{}
+}
+
+var SECRET_KEY = []byte("s3cr3t_k3y")
+
+func (s *jwtService) GenerateToken(userID int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
