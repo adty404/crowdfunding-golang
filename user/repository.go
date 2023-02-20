558c9597e09b1264ab9a38a8285b444309a64068
Aditya Prasetyo <adty404@gmail.com>
2023-02-20 09:32:43 +0700

    Upload Avatar API (Membuat Repository)

--- user/repository.go
+++ user/repository.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindByID(id int) (User, error)
+	Update(user User) (User, error)
 }
 
 type repository struct {
@@ -28,6 +30,25 @@ func (r *repository) Save(user User) (User, error) {
 func (r *repository) FindByEmail(email string) (User, error) {
 	var user User
 	err := r.db.Raw("SELECT * FROM users WHERE email = ?", email).Scan(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) FindByID(ID int) (User, error) {
+	var user User
+	err := r.db.Raw("SELECT * FROM users WHERE id = ?", ID).Scan(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error
 	if err != nil {
 		return user, err
 	}
