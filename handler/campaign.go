63ddeb467eb6cb63a295bb9c4238ed973275f8a9
Aditya Prasetyo <adty404@gmail.com>
2023-04-28 22:09:59 +0700

    Upload Campaign Image API (Membuat Handler)

--- handler/campaign.go
+++ handler/campaign.go
@@ -4,6 +4,7 @@ import (
 	"crowdfunding-golang/campaign"
 	"crowdfunding-golang/helper"
 	"crowdfunding-golang/user"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -119,5 +120,54 @@ func (h *campaignHandler) UpdateCampaign(c *gin.Context) {
 	}
 
 	response := helper.APIResponse("Update campaign success", http.StatusOK, "success", campaign.FormatCampaign(updatedCampaign))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *campaignHandler) UploadImage(c *gin.Context) {
+	var input campaign.CreateCampaignImageInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to update campaign image", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		data := gin.H{
+			"is_uplaoded" : false,
+		}
+		response := helper.APIResponse("Failed to upload campaign image!", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
+
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{
+			"is_uplaoded" : false,
+		}
+		response := helper.APIResponse("Failed to upload campaign image!", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	_, err = h.service.SaveCampaignImage(input, path)
+	if err != nil {
+		data := gin.H{
+			"is_uplaoded" : false,
+		}
+		response := helper.APIResponse("Failed to upload campaign image!", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+	}
+
+	data := gin.H{
+		"is_uplaoded" : true,
+	}
+	response := helper.APIResponse("Campaign image successfully uploaded!", http.StatusOK, "success", data)
 	c.JSON(http.StatusOK, response)
 }
